c35314e8cf464711999c23b446d8c610fa7e2e66
Sumer Patel <sumerdpatel@gmail.com>
2021-04-23 21:22:14 +0530

    added search box & card component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,96 @@
 <template>
   <div>
     <header>
-      Header
+      <h1>Vue<strong>Anime</strong></h1>
+      
+      <form class="search-box">
+        <input type="search" class="search-field" name="" id="" placeholder="Search for an Anime..." required />
+      </form>
     </header>
+    <main>
+      <div class="cards">
+        <Cards />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
+import Cards from './components/Cards';
 
 export default {
-  
+  setup() {
+    return {
+      Cards
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+a{
+  text-decoration: none;
+}
+header{
+  padding-top: 50px;
+  padding-bottom: 50px;
+  h1{
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+
+    strong{
+      color: #313131;
+    }
+    &:hover{
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+    align-items: center;
+
+    .search-field{
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+      background-color: #f3f3f3;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 10px;
+      color: #313131;
+      font-size: 1.2rem;
+      transition: 0.4s;
+
+      &::placeholder{
+        color: #aaa;
+      }
+
+      &:focus, &:valid {
+        color: #fff;
+        background-color: #313131;
+        box-shadow: 0 0 0 rgba(0, 0, 0, 0.15);
+      }
+    }
+  }
+}
 
 </style>
